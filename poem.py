3203323a5badbd98ea59113f858f9cd1e6a40912
Shivali <85780318+Shivali-Singh1@users.noreply.github.com>
2024-11-27 15:24:57 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,20 @@
+# Printing the "Twinkle Twinkle Little Star" poem
+
+print("Twinkle, twinkle, little star,")  # Line 1
+print("How I wonder what you are!")      # Line 2
+print("Up above the world so high,")     # Line 3
+print("Like a diamond in the sky.")     # Line 4
+print("Twinkle, twinkle, little star,")  # Line 5
+print("How I wonder what you are!")      # Line 6
+
+
+print ( "    ")
+
+# another way:
+print("another way")
+print("twinkle, twinkle,little star \n"
+      "How I wonder what you are! \n"
+    "Up above the world so high \n"
+      "Like a diamond in the sky.\n"
+     " Twinkle, twinkle, little star,\n"
+     "How I wonder what you are!" )
